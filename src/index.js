2c6f2e6b919835472aa2363f3d872acb7d4943e0
Daniel Alejandro Leon Ortiz <dalobusiness01@gmail.com>
2023-01-08 23:40:48 -0500

    Added css and improve some JS code: Added error message and loading message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// TODO: Add my own apiKey
-const apiKey = "d102ce6f8a7f8c61a416505fdeb98697";
+const apiKey = "3e04a6868539b1799be524e346d1b7c9";
 const cityApi = (city) => {
   return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 };
@@ -20,9 +19,15 @@ function getWeather(city) {
   xhttp.open("GET", url, true);
   xhttp.send();
 
+  xhttp.onprogress = () => {
+    result.innerHTML = "";
+    result.innerHTML = "<h1> Loading... </h1>";
+  };
+
   xhttp.onload = () => {
     if (xhttp.status != 200) {
-      alert(`Error ${xhttp.status}: ${xhttp.statusText}`);
+      result.innerHTML = "";
+      result.innerHTML = ` <h1>Error ${xhttp.status}: ${xhttp.statusText}</h1>`;
     } else {
       addWeatherToPage(JSON.parse(xhttp.responseText));
     }
@@ -34,7 +39,10 @@ function getWeather(city) {
 }
 
 function addWeatherToPage(data) {
+  result.innerHTML = "";
+
   const resultImg = document.createElement("img");
+  const resultInfo = document.createElement("div");
   const resultTemp = document.createElement("h1");
   const resultCity = document.createElement("h2");
   const resultClimate = document.createElement("h3");
@@ -44,6 +52,6 @@ function addWeatherToPage(data) {
   resultCity.innerText = data.name;
   resultClimate.innerText = data.weather[0].main;
 
-  result.innerHTML = "";
-  result.append(resultImg, resultTemp, resultCity, resultClimate);
+  resultInfo.append(resultTemp, resultCity, resultClimate);
+  result.append(resultImg, resultInfo);
 }
